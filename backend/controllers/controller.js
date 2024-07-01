8351b06ee5d96d9480f1036e49dd34f433895ae4
aswathi-48 <aswathik482@gmail.com>
2024-07-01 20:58:15 +0530

    quiz-app

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -2,3 +2,8 @@
 export async function  getQuestions (req,res){
     res.json("questions api get request");
 }
+
+
+export async function insertQuestions(req,res){
+    res.json("questions api post request")
+}
--- backend/router/route.js
+++ backend/router/route.js
@@ -1,16 +1,15 @@
 import {Router} from "express"
 const router =Router();
 
-// import controller from '../controllers/controller.js'
+import * as controller from '../controllers/controller.js'
 //Questions routes API
 
 
-router.get('/questions',(req,res)=>{
-    res.json("questions api get request")
-})
+// router.get('/questions', controller.getQuestions)
+// router.post('/questions', controller.insertQuestions)
+// change this step to
 
-
-
-
-
+router.route('/questions')
+    .get(controller.getQuestions)
+    .post(controller.insertQuestions)
 export default router;
